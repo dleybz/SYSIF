e6b7639465a6b87628dc0114755524853b879099
Corentin Kervadec HPC UPF <corentin.kervadec@wanadoo.fr>
2023-10-10 12:34:29 +0200

    Add args and fix bugs

--- fmap_main.py
+++ fmap_main.py
@@ -2,29 +2,55 @@ import torch
 import os
 import pickle
 import logging
+import argparse
+import random
+
 from src.fmap.fmap import extract_fmap
 from src.data.dataset_loader import load_hf_dataset_with_sampling
 from src.model.causal_lm import CausalLanguageModel
+from src.utils.init_utils import init_device, init_random_seed
 
+def parse_args():
+    parser = argparse.ArgumentParser(description='OPTCorpus generation')
 
+    # Data selection
+    parser.add_argument('--model_name', type=str, default="EleutherAI/pythia-70m-deduped")
+    parser.add_argument('--dataset', type=str, default='wikipedia,20220301.en,train')
+    parser.add_argument('--batch_size', type=int, default=8)
+    parser.add_argument('--seed', type=int, default=-1)
+    parser.add_argument('--n_samples', type=int, default=100000)
+    parser.add_argument('--window_size', type=int, default=15)
+    parser.add_argument('--window_stride', type=int, default=15)
+    parser.add_argument('--device', type=str, default='cuda', help='Which computation device: cuda or mps')
+    parser.add_argument('--output_dir', type=str, default='./unit-token-analyze', help='the output directory to store prediction results')
+    parser.add_argument('--fp16', action='store_true', help='use half precision')
+    args = parser.parse_args()
+    print(args)
+
+    return args
 
 if __name__ == "__main__":
 
-    name = "wikitext,wikitext-103-raw-v1,train"#'wikipedia,20220301.en,train'
-    dataset = load_hf_dataset_with_sampling(name, n_samples=100)
+    args = parse_args()
+    
+    random_seed = init_random_seed(args.seed)
+    init_device(args.device)
 
-    model_name = "EleutherAI/pythia-70m-deduped"  # You can replace this with the model name you want to use
-    model = CausalLanguageModel(model_name, device="cuda" if torch.cuda.is_available() else "cpu")
+    dataset = load_hf_dataset_with_sampling(args.dataset, n_samples=args.n_samples)
+
+    model_name = args.model_name
+    model = CausalLanguageModel(model_name, device="cuda" if torch.cuda.is_available() else "cpu", fp16=args.fp16)
 
     mode = ['input', 'output']
 
     unit_tokens_accum, tokens_count = extract_fmap(model=model, 
                  dataset=dataset,
-                 batch_size=32,
-                 window_size=15,
-                 window_stride=15,
-                 device='cpu',
-                 mode=mode)
+                 batch_size=args.batch_size,
+                 window_size=args.window_size,
+                 window_stride=args.window_stride,
+                 device=args.device,
+                 mode=mode, 
+                 fp16=args.fp16)
     
     # safety check
     warning_flag = ''
@@ -46,9 +72,9 @@ if __name__ == "__main__":
     
     # Save with pickle
     print('Saving stats...')
-    exp_name = f'test_wikitext'
+    exp_name = f'{args.model_name.split("/")[-1]}-N{args.n_samples}-{random_seed}'
     exp_name += '_'+warning_flag
-    save_dir = os.path.join('./',f'unit-token-wiki')
+    save_dir = os.path.join(args.output_dir,f'unit-token-wiki.{random_seed}')
 
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
